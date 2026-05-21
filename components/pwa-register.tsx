88322deea0bbfed6a8f0bae697a6a69a6e6309f6
ezequieldanza93-tech <hhss@amarilla.com.ar>
2026-05-21 14:54:34 -0300

    fix(pwa): add id/scope to manifest + explicit SW scope for Android Chrome installability

--- components/pwa-register.tsx
+++ components/pwa-register.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 export function PWARegister() {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker')
+      navigator.serviceWorker.register('/service-worker', { scope: '/' })
     }
   }, [])
 
